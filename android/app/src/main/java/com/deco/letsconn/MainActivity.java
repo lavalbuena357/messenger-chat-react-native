7a4e8278fb97643d4b345f8ca9492c6ce06bc597
lavalbuena357 <cuervo357@gmail.com>
2022-05-15 13:14:11 -0500

    dependencias instaladas y splash screen inicial

--- android/app/src/main/java/com/deco/letsconn/MainActivity.java
+++ android/app/src/main/java/com/deco/letsconn/MainActivity.java
@@ -1,8 +1,10 @@
 package com.deco.letsconn;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import org.devio.rn.splashscreen.SplashScreen;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +15,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "LetsConn";
+  }
+
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);  // here
+      super.onCreate(savedInstanceState);
   }
 
   /**
