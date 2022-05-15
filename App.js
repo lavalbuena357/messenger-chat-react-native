c92707a5b6bcd9710ed4f7cd956dd2b14fe48dfc
lavalbuena357 <cuervo357@gmail.com>
2022-05-15 18:38:21 -0500

    configuracion de tema personalizado

--- App.js
+++ App.js
@@ -1,18 +1,35 @@
-import { View, Text, StatusBar } from 'react-native'
-import React from 'react'
+import { StatusBar } from 'react-native'
+import React, { useEffect } from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
-import Theme, { color } from './src/colors/colors'
+import Theme from './src/colors/colors'
 import Splash from './src/screens/Splash/Splash'
 import Login from './src/screens/Login/Login'
 import Main from './src/screens/Main/Main'
+import { useDispatch, useSelector } from 'react-redux'
+import { getTheme } from './src/redux/actions'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const Stack = createNativeStackNavigator()
 
 const App = () => {
+
+  const dispatch = useDispatch()
+  const theme = useSelector(state => state.theme)
+
+  useEffect(() => {
+    (async() => {
+      const themeStorage = await AsyncStorage.getItem('theme')
+      if(themeStorage) {
+        dispatch(getTheme(themeStorage))
+      }
+    })()
+  }, [])
+  console.log(Theme[theme])
+
   return (
-    <NavigationContainer theme={Theme}>
-      <StatusBar backgroundColor={color.primary} />
+    <NavigationContainer theme={theme === 'dark' ? Theme.dark : Theme.light}>
+      <StatusBar backgroundColor={Theme[theme].colors.statusBar} />
       <Stack.Navigator initialRouteName='Splash'>
         <Stack.Screen
           name='Splash'
