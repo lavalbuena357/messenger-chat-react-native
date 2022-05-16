a05e0e9c8a9a672f45f8e399c51cbc72b38764ee
lavalbuena357 <cuervo357@gmail.com>
2022-05-15 22:53:08 -0500

    se añadieron las tabs al componente main

--- src/screens/Main/Main.js
+++ src/screens/Main/Main.js
@@ -1,12 +1,37 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import Header from '../../components/Header/Header'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import TabBar from '../../components/TabBar/TabBar'
+import Rooms from '../Rooms/Rooms'
+import Contacts from '../Contacts/Contacts'
+
+const Tab = createBottomTabNavigator()
 
 const Main = () => {
+
   return (
-    <View>
-      <Header />
-    </View>
+    <Tab.Navigator
+      initialRouteName='Rooms'
+      tabBar={props => <TabBar {...props} />} >
+      <Tab.Screen
+        name='Rooms'
+        component={Rooms}
+        options={{
+          headerShown: false,
+          tabBarIcon: 'chatbubble-outline',
+          tabBarLabel:'Rooms',
+          tabBarBadge: 2,
+        }} />
+      <Tab.Screen
+        name='Contactos'
+        component={Contacts}
+        options={{
+          headerShown: false,
+          tabBarIcon: 'md-people-outline',
+          tabBarLabel:'Contactos',
+          tabBarBadge: 0,
+        }} />
+    </Tab.Navigator>
   )
 }
 
