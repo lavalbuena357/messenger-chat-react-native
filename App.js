f9e1907591323098eb2e9828a03e584ffaf3c963
lavalbuena357 <cuervo357@gmail.com>
2022-05-15 22:53:39 -0500

    rutas nuevas en app

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import { StatusBar } from 'react-native'
 import React, { useEffect } from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useDispatch, useSelector } from 'react-redux'
 import Theme from './src/colors/colors'
 import Splash from './src/screens/Splash/Splash'
 import Login from './src/screens/Login/Login'
 import Main from './src/screens/Main/Main'
-import { useDispatch, useSelector } from 'react-redux'
+import Rooms from './src/screens/Rooms/Rooms'
+import Contacts from './src/screens/Contacts/Contacts'
 import { getTheme } from './src/redux/actions'
-import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const Stack = createNativeStackNavigator()
 
@@ -25,7 +27,6 @@ const App = () => {
       }
     })()
   }, [])
-  console.log(Theme[theme])
 
   return (
     <NavigationContainer theme={theme === 'dark' ? Theme.dark : Theme.light}>
@@ -42,6 +43,14 @@ const App = () => {
         <Stack.Screen
           name='Main'
           component={Main}
+          options={{headerShown: false}} />
+        <Stack.Screen
+          name='Rooms'
+          component={Rooms}
+          options={{headerShown: false}} />
+        <Stack.Screen
+          name='Contacts'
+          component={Contacts}
           options={{headerShown: false}} />
       </Stack.Navigator>
       
