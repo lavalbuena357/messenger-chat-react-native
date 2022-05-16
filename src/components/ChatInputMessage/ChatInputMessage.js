3e655310ad4fd218d2b18f595d92bc36a820165a
lavalbuena357 <cuervo357@gmail.com>
2022-05-16 13:37:17 -0500

    componente de input del chat creado

---
+++ src/components/ChatInputMessage/ChatInputMessage.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ChatInputMessage = () => {
+  return (
+    <View>
+      <Text>ChatInputMessage</Text>
+    </View>
+  )
+}
+
+export default ChatInputMessage
