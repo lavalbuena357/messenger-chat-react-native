50161711f778b21e4f6ba7dd533dbb75bcf998f9
lavalbuena357 <cuervo357@gmail.com>
2022-05-16 18:32:56 -0500

    actualizacion del componente de chats

--- src/screens/ChatScreen/ChatScreen.js
+++ src/screens/ChatScreen/ChatScreen.js
@@ -1,19 +1,35 @@
-import { View, Text } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, KeyboardAvoidingView, ScrollView } from 'react-native'
+import React, { useRef, useState } from 'react'
 import Loader from '../../components/Loader/Loader'
 import useStyles from './ChatScreen.styles'
 import HeaderChat from '../../components/HeaderChat/HeaderChat'
+import ChatInputMessage from '../../components/ChatInputMessage/ChatInputMessage'
 
 const ChatScreen = () => {
   const [isLoading, setIsLoading] = useState(false)
 
   const styles = useStyles()
+  const scrollViewRef = useRef()
 
   return (
     <View style={styles.container}>
       {isLoading ? <Loader color={styles.loading.color} size={60} /> :
-      <View>
+      <View style={styles.container}>
         <HeaderChat />
+        <KeyboardAvoidingView
+          behavior={null}
+          keyboardVerticalOffset={70}
+          style={styles.content} >
+          <View>
+            <ScrollView
+              ref={scrollViewRef}
+              onLayout={() => scrollViewRef.current.scrollToEnd({animated: true})}
+              onContentSizeChange={() => scrollViewRef.current.scrollToEnd({animated: true})} >
+              <Text>text</Text>
+            </ScrollView>
+          </View>
+        </KeyboardAvoidingView>
+        <ChatInputMessage />
       </View>
       }
     </View>
