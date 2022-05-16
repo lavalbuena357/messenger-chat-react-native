7472fe029d919a982a8fa8202f36d61d74d85c11
lavalbuena357 <cuervo357@gmail.com>
2022-05-15 22:52:49 -0500

    componentes de contactos y rooms

---
+++ src/screens/Rooms/Rooms.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Header from '../../components/Header/Header'
+
+const Rooms = () => {
+  return (
+    <View>
+      <Header />
+    </View>
+  )
+}
+
+export default Rooms
