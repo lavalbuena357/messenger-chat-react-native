19843f5ef04dd6c10512ec34d743d2ab15b0a8ad
lavalbuena357 <cuervo357@gmail.com>
2022-05-17 13:04:17 -0500

    se creo el componente contact detail para ver los datos de contacto y editarlo

---
+++ src/screens/ContactDetail/ContactDetail.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ContactDetail = () => {
+  return (
+    <View>
+      <Text>ContactDetail</Text>
+    </View>
+  )
+}
+
+export default ContactDetail
