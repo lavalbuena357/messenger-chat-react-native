ea84e27e8027c0cc9e0786fd576d29198fde84a2
lavalbuena357 <cuervo357@gmail.com>
2022-05-17 23:18:23 -0500

    se creo la screen contacts manager

---
+++ src/screens/ContactsManager/ContactsManager.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import useStyles from './ContactsManager.styles'
+
+const ContactsManager = () => {
+
+  const styles = useStyles()
+
+  return (
+    <View>
+      <Text>ContactsManager</Text>
+    </View>
+  )
+}
+
+export default ContactsManager
