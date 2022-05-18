26971eb7c26fd3c2727b6807f39e9d62b70c0f71
lavalbuena357 <cuervo357@gmail.com>
2022-05-18 15:25:11 -0500

    custom touchable para iconos

--- src/components/ModalTouchableCustom/ModalTouchableCustom.js
+++ src/components/ModalTouchableCustom/ModalTouchableCustom.js
@@ -1,14 +1,24 @@
 import { Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import useStyles from '../ModalTemplate/ModalTemplate.styles'
+import Icon from 'react-native-vector-icons/FontAwesome5'
 
-const ModalTouchableCustom = ({handleFunction, buttonName, type}) => {
+const ModalTouchableCustom = ({handleFunction, buttonName, type, iconName}) => {
   const styles = useStyles()
 
   return (
-    <TouchableOpacity onPress={handleFunction} style={styles.textButton}>
-      {type === 'text' && <Text style={styles.textButtonText}>{buttonName}</Text>}
-    </TouchableOpacity>
+    <>
+      {type === 'text' ? 
+      <TouchableOpacity onPress={handleFunction} style={type === 'text' ? styles.textButton: styles.iconButton}>
+        <Text style={styles.textButtonText}>{buttonName}</Text>
+      </TouchableOpacity>
+      :
+      <TouchableOpacity onPress={handleFunction} style={type === 'text' ? styles.textButton: styles.iconButton}>
+        <Icon name={iconName} color={styles.iconButtonText.color} size={24} />
+        <Text style={styles.iconButtonText}>{buttonName}</Text>
+      </TouchableOpacity>
+      }
+    </>
   )
 }
 
