b6426c710885edf8a50cbd5af27edb22f06943d7
lavalbuena357 <cuervo357@gmail.com>
2022-05-17 23:12:53 -0500

    se creo la screen my profile

---
+++ src/screens/MyProfile/MyProfile.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import useStyles from './MyProfile.styles'
+
+const MyProfile = () => {
+
+  const styles = useStyles()
+
+  return (
+    <View>
+      <Text>MyProfile</Text>
+    </View>
+  )
+}
+
+export default MyProfile
---
+++ src/screens/MyProfile/MyProfile.styles.js
@@ -0,0 +1,31 @@
+import { useMemo } from 'react';
+import { StyleSheet } from 'react-native'
+import { useSelector } from 'react-redux';
+import Theme from '../../colors/colors'
+
+const getStyles = ({colors}) => StyleSheet.create({
+  container: {
+    
+  },
+  icon: {
+    color: colors.text,
+    marginLeft: 15,
+    padding: 5,
+    backgroundColor: colors.secondary,
+    borderRadius: 5
+  },
+  loader: {
+    color: colors.white
+  }
+})
+
+const useStyles = () => {
+  const theme = useSelector(state => state.theme)
+  const { colors } = Theme[theme]
+
+  const styles = useMemo(() => getStyles({colors}), [colors])
+
+  return styles
+}
+
+export default useStyles
