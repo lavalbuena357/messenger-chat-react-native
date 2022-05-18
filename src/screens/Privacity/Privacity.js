459affd718dcd8904afe28c6fd99c6b1eb67f7f1
lavalbuena357 <cuervo357@gmail.com>
2022-05-17 23:14:34 -0500

    se creo la screen privacity

---
+++ src/screens/Privacity/Privacity.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import useStyles from './Privacity.styles'
+
+const Privacity = () => {
+
+  const styles = useStyles()
+
+  return (
+    <View>
+      <Text>Privacity</Text>
+    </View>
+  )
+}
+
+export default Privacity
