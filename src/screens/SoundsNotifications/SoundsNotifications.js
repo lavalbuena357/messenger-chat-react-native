344f9b9e277152295211a8b5aeb56e7fbde97396
lavalbuena357 <cuervo357@gmail.com>
2022-05-17 23:16:15 -0500

    se creo la screen sounds and notifications

---
+++ src/screens/SoundsNotifications/SoundsNotifications.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import useStyles from '../MyProfile/MyProfile.styles'
+
+const SoundsNotifications = () => {
+
+  const styles = useStyles()
+
+  return (
+    <View>
+      <Text>SoundsNotifications</Text>
+    </View>
+  )
+}
+
+export default SoundsNotifications
