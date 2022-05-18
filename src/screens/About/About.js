ae2b89fa6479cda501a6377778161fe1ef11e6d7
lavalbuena357 <cuervo357@gmail.com>
2022-05-17 23:19:42 -0500

    se creo la screen about

---
+++ src/screens/About/About.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import useStyles from './About.styles'
+
+const About = () => {
+
+  const styles = useStyles()
+
+  return (
+    <View>
+      <Text>About</Text>
+    </View>
+  )
+}
+
+export default About
