77545bdb87e3a4dbdd4ce37c836908185e4b7c9f
lavalbuena357 <cuervo357@gmail.com>
2022-05-19 00:17:43 -0500

    se implemento la interfaz de privacidad

--- src/screens/Privacity/Privacity.js
+++ src/screens/Privacity/Privacity.js
@@ -1,14 +1,89 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import Icon from 'react-native-vector-icons/Ionicons'
+import IconFAw from 'react-native-vector-icons/FontAwesome5'
 import useStyles from './Privacity.styles'
 
-const Privacity = () => {
+const Privacity = ({navigation}) => {
+  const [showHiddenAll, setShowHiddenAll] = useState(false)
+  const [showHiddenOnline, setShowHiddenOnline] = useState(false)
+  const [showHiddenStatus, setShowHiddenStatus] = useState(false)
+  const [showHiddenPhoto, setShowHiddenPhoto] = useState(false)
+  const [showHiddeEmail, setShowHiddeEmail] = useState(false)
 
   const styles = useStyles()
 
+  const handleAll = () => {
+    if(showHiddenAll) {
+      setShowHiddeEmail(false)
+      setShowHiddenOnline(false)
+      setShowHiddenPhoto(false)
+      setShowHiddenStatus(false)
+    } else {
+      setShowHiddeEmail(true)
+      setShowHiddenOnline(true)
+      setShowHiddenPhoto(true)
+      setShowHiddenStatus(true)
+    }
+    setShowHiddenAll(!showHiddenAll)
+  }
+
+  const handleOnline = () => {
+    setShowHiddenOnline(!showHiddenOnline)
+  }
+
+  const handleStatus = () => {
+    setShowHiddenStatus(!showHiddenStatus)
+  }
+
+  const handlePhoto = () => {
+    setShowHiddenPhoto(!showHiddenPhoto)
+  }
+
+  const handleEmail = () => {
+    setShowHiddeEmail(!showHiddeEmail)
+  }
+
   return (
-    <View>
-      <Text>Privacity</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack(null)}>
+          <Icon name='arrow-back' size={30} color={styles.headerTitle.color} />
+        </TouchableOpacity>
+        <Text style={styles.headerTitle}>Privacidad</Text>
+      </View>
+      <View style={styles.optionsContainer}>
+        <View style={styles.itemContainerAll}>
+          <Text style={styles.itemTextAll}>Mostrar/Ocultar todo</Text>
+          <TouchableOpacity onPress={handleAll}>
+            <IconFAw name={showHiddenAll ? 'toggle-on' : 'toggle-off'} size={30} color={styles.itemText.color} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.itemContainer}>
+          <Text style={styles.itemText}>Mostrar en línea</Text>
+          <TouchableOpacity onPress={handleOnline}>
+            <IconFAw name={showHiddenOnline ? 'toggle-on' : 'toggle-off'} size={30} color={styles.itemText.color} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.itemContainer}>
+          <Text style={styles.itemText}>Estado para mostrar</Text>
+          <TouchableOpacity onPress={handleStatus}>
+            <IconFAw name={showHiddenStatus ? 'toggle-on' : 'toggle-off'} size={30} color={styles.itemText.color} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.itemContainer}>
+          <Text style={styles.itemText}>Mostrar foto de perfil</Text>
+          <TouchableOpacity onPress={handlePhoto}>
+            <IconFAw name={showHiddenPhoto ? 'toggle-on' : 'toggle-off'} size={30} color={styles.itemText.color} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.itemContainer}>
+          <Text style={styles.itemText}>Mostrar mi correo</Text>
+          <TouchableOpacity onPress={handleEmail}>
+            <IconFAw name={showHiddeEmail ? 'toggle-on' : 'toggle-off'} size={30} color={styles.itemText.color} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   )
 }
--- src/screens/Privacity/Privacity.styles.js
+++ src/screens/Privacity/Privacity.styles.js
@@ -5,17 +5,50 @@ import Theme from '../../colors/colors'
 
 const getStyles = ({colors}) => StyleSheet.create({
   container: {
-    
+    flex: 1,
   },
-  icon: {
-    color: colors.text,
-    marginLeft: 15,
-    padding: 5,
-    backgroundColor: colors.secondary,
-    borderRadius: 5
+  header: {
+    height: 70,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: colors.primary,
+    elevation: 3,
+    shadowColor: colors.shadow
   },
-  loader: {
-    color: colors.white
+  backButton: {
+    padding: 5
+  },
+  headerTitle: {
+    color: colors.textBody,
+    marginLeft: 10,
+    fontSize: 18
+  }, 
+  optionsContainer: {
+    marginHorizontal: 20,
+    marginVertical: 25,
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 10
+  },
+  itemText: {
+    color: colors.textBody
+  },
+  itemContainerAll: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingBottom: 5,
+    marginBottom: 15,
+    borderBottomColor: colors.secondary,
+    borderBottomWidth: 0.5
+  },
+  itemTextAll: {
+    color: colors.textBody,
+    fontWeight: 'bold',
+    fontSize: 16
   }
 })
 
