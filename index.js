6d3ed48abfa5c5b5bc355c0e5ce0105dcfc05611
lavalbuena357 <cuervo357@gmail.com>
2022-05-19 20:42:15 -0500

    se añadio time ago con configuracion en español

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ import { Provider } from 'react-redux'
 import {AppRegistry} from 'react-native';
 import App from './App';
 import store from './src/redux/store'
+import TimeAgo from 'javascript-time-ago'
+import es from 'javascript-time-ago/locale/es-CO.json'
 import {name as appName} from './app.json';
+
+TimeAgo.addDefaultLocale(es)
+TimeAgo.addLocale(es)
 
 const Root = () => (
   <Provider store={store}>
