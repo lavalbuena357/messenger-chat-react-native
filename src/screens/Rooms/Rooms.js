e3bedb7caea3f11af5e1ad88081bad70039a52d8
lavalbuena357 <cuervo357@gmail.com>
2022-05-19 20:40:40 -0500

    se añadieron todos los chats en la pagina principal rrom

--- src/screens/Rooms/Rooms.js
+++ src/screens/Rooms/Rooms.js
@@ -1,15 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text,ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Header from '../../components/Header/Header'
 import Loader from '../../components/Loader/Loader'
 import useStyles from './Rooms.styles'
+import ChatItem from '../../components/ChatItem/ChatItem'
 
 const Rooms = ({navigation}) => {
   const [isLoading, setIsLoading] = useState(true)
+  const [showActionsModal, setShowActionsModal] = useState(false)
+  const [uidSelected, setUidSelected] = useState('')
 
   const styles = useStyles()
-  const {currentUser} = useSelector(state => state)
+  const {currentUser, chats} = useSelector(state => state)
 
   useEffect(() => {
     setIsLoading(true)
@@ -18,10 +21,35 @@ const Rooms = ({navigation}) => {
     }
   }, [currentUser])  
 
+  const handleSelected = (uid) => {
+
+  }
+
+  const handleGoToContactChat = (uid) => {
+
+  }
+
   return (
     <View style={styles.container}>
       {isLoading ? <Loader color={styles.loading.color} size={60} /> :
-      <Header />
+      <View>
+        <Header />
+        {chats.length ?
+        <ScrollView>
+          {chats.map(el => (
+            <ChatItem 
+              key={el.chatId} 
+              chat={el}
+              myUid={currentUser.uid}
+              uidSelected={uidSelected}
+              handleSelected={handleSelected}
+              handleGoToContactChat={handleGoToContactChat} />
+          ))}
+        </ScrollView>
+        :
+        <Text style={styles.notFound}>La lista de contactos esta vacía</Text>
+        }
+      </View>
       }
     </View>
   )
