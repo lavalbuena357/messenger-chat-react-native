d493a7918271dac14c59d6a8fdb469b7e9b897cb
lavalbuena357 <cuervo357@gmail.com>
2022-05-29 13:35:27 -0500

    se agrego la funcion para calcular el tamaño de la imagen

--- src/components/ChatScreen/ChatModalImage/ModalPreview.js
+++ src/components/ChatScreen/ChatModalImage/ModalPreview.js
@@ -8,18 +8,25 @@ import { useSelector } from 'react-redux'
 
 const ModalPreview = ({setIsModalPreview, isModalPreview, mediaData, contact}) => {
 
-  const { width, height } = useWindowDimensions()
+  const { width: screenWidth, height: screenHeight } = useWindowDimensions()
   const styles = useStyles(getStyles)
   const {currentUser} = useSelector(state => state)
 
+  const resize = (d) => {
+    const aspectRatio = d.height > d.width ? '0.5' : d.width > d.height ? '1.5' : '1'
+    const width = d.width > d.height ? `${screenWidth*0.95}` : d.width === d.height ? `${screenWidth*0.95}` : 'null'
+    const height = d.height > d.width ? `${screenHeight*0.8}` : 'null'
+    return {aspectRatio, width, height}
+  }
+
   const uploadFile = async() => {
-    console.log(mediaData)
     const file = mediaData[0][0].base64
+    const metadata = resize({width: mediaData[0][0].width, height: mediaData[0][0].height})
     const cate = mediaData[1]
     const timestamp = Date.now()
     const fileExtension = mediaData[0][0].fileName.split('.')[1]
     const filename = `${cate}_${currentUser.uid}-${timestamp}.${fileExtension}`
-    saveMedia(file, filename, cate, currentUser.uid, contact.uid)
+    saveMedia(file, metadata, filename, cate, currentUser.uid, contact.uid)
     setIsModalPreview(false)
   }
 
@@ -43,8 +50,8 @@ const ModalPreview = ({setIsModalPreview, isModalPreview, mediaData, contact}) =
             style={{
               borderRadius: 10, 
               aspectRatio: el.height > el.width ? 0.5 : el.width > el.height ? 1.5 : 1, 
-              width: el.width > el.height ? width*0.95: el.width === el.height ? width*0.95 : null, 
-              height: el.height > el.width ? height*0.8 : null}} />
+              width: el.width > el.height ? screenWidth*0.95: el.width === el.height ? screenWidth*0.95 : null, 
+              height: el.height > el.width ? screenHeight*0.8 : null}} />
         ))}
         <View style={styles.boxButtons}>
           <TouchableOpacity style={styles.ButtonPreview} onPress={() => setIsModalPreview(false)}>
